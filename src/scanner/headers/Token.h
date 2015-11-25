ab05ace4a08d13fc23a3a2f3eab15fb4003bf8a1
daB0bby <gena.eirich@gmail.com>
2015-11-25 12:36:46 +0100

    - fix merge conflicts

--- src/scanner/headers/Token.h
+++ src/scanner/headers/Token.h
@@ -2,14 +2,13 @@
 // Created by tim on 04.11.15.
 //
 
-#ifndef HSKA_COMPILER_TOKEN_H
-#define HSKA_COMPILER_TOKEN_H
+#ifndef TOKEN_H
+#define TOKEN_H
 
 enum TType {
     //TODO: Add more Token Types
     IDENTIFIER_TYPE,
     OPERATOR_TYPE,
-
     ERROR_TYPE
 };
 
@@ -18,8 +17,8 @@ private:
     TType type;
     int line;
     int column;
-    //TODO: Ref to class Information?
+    // TODO: Ref to class Information?
 };
 
 
-#endif //HSKA_COMPILER_TOKEN_H
+#endif /* TOKEN_H */
