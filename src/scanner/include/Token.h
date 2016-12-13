681a80b3386f7f42389b5e44daf3beebb989928e
daB0bby <gena.eirich@gmail.com>
2016-12-13 23:23:18 +0100

    Updates Token and EOF was added

--- src/scanner/include/Token.h
+++ src/scanner/include/Token.h
@@ -2,86 +2,119 @@
 // Created by tim on 04.11.15.
 //
 
+
 #ifndef TOKEN_H
 #define TOKEN_H
 
-class Token 
+
+class Token
 {
-public:
-	enum TokenType
-	{
-        NONE,
-
-		PLUS,
-		MINUS,
-		STAR,
-		COLON,
-		IF,
-		WHILE,
-		INTEGER,
-		IDENTIFIER,
-		SEMICOLON,
-		GREATER,
-		LESS,
-		EQUAL,
-		ASSIGN,
-		UNKNOWN_BULLSHIT_OPERATOR,
-		NOT,
-		AND,
-		BRACKET_OPEN,
-		BRACKET_CLOSE,
-		SQUARE_BRACKET_OPEN,
-		SQUARE_BRACKET_CLOSE,
-		CURLY_BRACKET_OPEN,
-		CURLY_BRACKET_CLOSE,
-        IGNORE,                     // white spaces and comments
-		ERROR,
-
-        STATE_TBD,      // special state (no valid token found right now)
-        STATE_FAILED    // special state (no valid token can be found any further)        
-	};
-
-    static const char* getTokenName(int type)
-    {
-        const static char* tokenNames[] =
+    public:
+        enum TokenType
         {
-            "NONE",
+            NONE,
+            EOF,    // End of file token
 
-            "PLUS",
-            "MINUS",
-            "STAR",
-            "COLON",
-            "IF",
-            "WHILE",
-            "INTEGER",
-            "IDENTIFIER",
-            "SEMICOLON",
-            "GREATER",
-            "LESS",
-            "EQUAL",
-            "ASSIGN",
-            "UNKNOWN_BULLSHIT_OPERATOR",
-            "NOT",
-            "AND",
-            "BRACKET_OPEN",
-            "BRACKET_CLOSE",
-            "SQUARE_BRACKET_OPEN",
-            "SQUARE_BRACKET_CLOSE",
-            "CURLY_BRACKET_OPEN",
-            "CURLY_BRACKET_CLOSE",
-            "IGNORE",                     // white spaces and comments
-            "ERROR",
+            PLUS,
+            MINUS,
+            STAR,
+            COLON,
+            IF,
+            WHILE,
+            INTEGER,
+            IDENTIFIER,
+            SEMICOLON,
+            GREATER,
+            LESS,
+            EQUAL,
+            ASSIGN,
+            UNKNOWN_BULLSHIT_OPERATOR,
+            NOT,
+            AND,
+            BRACKET_OPEN,
+            BRACKET_CLOSE,
+            SQUARE_BRACKET_OPEN,
+            SQUARE_BRACKET_CLOSE,
+            CURLY_BRACKET_OPEN,
+            CURLY_BRACKET_CLOSE,
+            IGNORE,                     // white spaces and comments
+            ERROR,
 
-            "STATE_TBD",      // special state (no valid token found right now)
-            "STATE_FAILED"    // special state (no valid token can be found any further)        
+            STATE_TBD,      // special state (no valid token found right now)
+            STATE_FAILED    // special state (no valid token can be found any further)
         };
 
-        return tokenNames[type];
-    }
+        Token(TokenType type, const int& line, const int& column);
+        ~Token();
 
-    TokenType type;
-    int line;
-    int column;  
+        /**
+         * Return TokenType
+         * @return
+         */
+        TokenType getType() const { return _type; }
+
+        /**
+         * Returns line number where token was found.
+         * @return
+         */
+        int getLine() const { return _line; }
+
+        /**
+         * Returns column number where token was found
+         * @return
+         */
+        int getColumn() const { return _column; }
+
+        /**
+         * Return TokenType string to given type
+         *
+         * @param type
+         * @return
+         */
+        static const char* getTokenName(int type)
+        {
+            const static char* tokenNames[] =
+            {
+                "NONE",
+                "EOF",
+
+                "PLUS",
+                "MINUS",
+                "STAR",
+                "COLON",
+                "IF",
+                "WHILE",
+                "INTEGER",
+                "IDENTIFIER",
+                "SEMICOLON",
+                "GREATER",
+                "LESS",
+                "EQUAL",
+                "ASSIGN",
+                "UNKNOWN_BULLSHIT_OPERATOR",
+                "NOT",
+                "AND",
+                "BRACKET_OPEN",
+                "BRACKET_CLOSE",
+                "SQUARE_BRACKET_OPEN",
+                "SQUARE_BRACKET_CLOSE",
+                "CURLY_BRACKET_OPEN",
+                "CURLY_BRACKET_CLOSE",
+                "IGNORE",                     // white spaces and comments
+                "ERROR",
+
+                "STATE_TBD",      // special state (no valid token found right now)
+                "STATE_FAILED"    // special state (no valid token can be found any further)
+            };
+
+            return tokenNames[type];
+        }
+
+    private:
+        TokenType _type;
+        const int _line;
+        const int _column;
+
 };
 
 
---
+++ src/scanner/src/Token.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Bob on 13.12.2016.
+//
+
+
+#include "../include/Token.h"
+
+
+Token::Token(TokenType type, const int &line, const int &column)  :
+    _type(type),
+    _line(line),
+    _column(column)
+{
+
+}
+
+
+Token::~Token()
+{
+
+}
