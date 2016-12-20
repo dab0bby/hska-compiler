16271036b30ca6493637f55a9aeb1604499867f5
daB0bby <gena.eirich@gmail.com>
2016-12-20 23:00:46 +0100

    Removes trailing whitespaces

--- src/state-machine/src/StateMachine_Test.cpp
+++ src/state-machine/src/StateMachine_Test.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 int main ( int argc, char* argv[] )
-{    
     auto path = "E:/Code/hska-compiler/test-files/scanner1.txt";
+{
     LanguageParser lp;
 
     ifstream file(path);
@@ -19,29 +19,29 @@ int main ( int argc, char* argv[] )
     string str;
     int token;
 
-    while (getline(file, str))    
+    while (getline(file, str))
         fileContent << str << "\n";
-    
+
     // add some null terminator
     testString = fileContent.str() + '\0' + '\0' + '\0' + '\0';
 
     auto benchmarkStart = std::chrono::high_resolution_clock::now();
 
     cout << "testing state-machine with input file: " << path << endl << endl;
-    
+
     for (int i = 0; i <= testString.size(); i++)
     {
         auto c = testString[i];
-        
+
         bool parsed = lp.parse(c);
 
         token = lp.getToken();
-           
+
     //    if (token != Token::DETECTING && token != Token::IGNORE)
             cout << Token::getTokenName(token) << ", ";
 
         if (token == Token::NEW_LINE)
-            cout << endl << endl;     
+            cout << endl << endl;
 
         if (!parsed)
         {
@@ -55,7 +55,7 @@ int main ( int argc, char* argv[] )
                 cout << endl << endl;
         }
     }
-   
+
     auto benchmarkEnd = std::chrono::high_resolution_clock::now();
     auto time = benchmarkEnd - benchmarkStart;
 
